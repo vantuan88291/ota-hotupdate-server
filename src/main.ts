8ead1d29817bdd567dab04cda1f6c36d7455bc9b
Loc Nguyen <nguyentloc99@gmail.com>
2024-12-26 22:00:34 +0800

    Add swagger to API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  configureSwagger(app);
   await app.listen(3000);
 }
+
+function configureSwagger(app: INestApplication) {
+
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API Documentation for My Application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+}
+
 bootstrap();
--- src/modules/version-management/version-management.controller.ts
+++ src/modules/version-management/version-management.controller.ts
@@ -2,9 +2,26 @@ import { Body, Controller, Get, Post, Query, Req, UploadedFile, UseInterceptors 
 import { VersionManagementService } from './version-management.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Request } from 'express';
+import { ApiBody, ApiConsumes, ApiProperty, ApiQuery, ApiResponse } from '@nestjs/swagger';
 
-interface CreateNewVersionBody {
+class CreateNewVersionBody {
+	@ApiProperty({ description: "New Version"})
 	versionName: string;
+
+	@ApiProperty({
+		description: 'Compressed bundle',
+		type: 'string',
+		format: 'binary',
+	})
+	bundle: any;
+}
+
+class CreateNewVersionDTO {
+}
+
+class GetVersionUpdateDTO {
+	@ApiProperty({ description: "Link to fetch the asset bundle" })
+	assetBundle?: string;
 }
 
 @Controller()
@@ -12,22 +29,27 @@ export class VersionManagementController {
 	constructor(private readonly versionManagementService: VersionManagementService) { }
 
 	@Post("versions")
+	@ApiResponse({status: 200, type: CreateNewVersionDTO})
+	@ApiConsumes('multipart/form-data')
+	@ApiBody({type: CreateNewVersionBody})
 	@UseInterceptors(FileInterceptor('bundle'))
 	createNewVersion(
 		@Body() body: CreateNewVersionBody,
 		@UploadedFile() bundle: Express.Multer.File
-	) {
+	): Promise<CreateNewVersionDTO> {
 		return this.versionManagementService.createNewVersion({
 			versionName: body.versionName,
 			bundle: bundle.buffer
-		});
+		}).then(() => ({}));
 	}
 
 	@Get("version/update")
+	@ApiQuery({ name: 'version', description: 'Version of the app' })
+	@ApiResponse({status: 200, type: GetVersionUpdateDTO})
 	getVersionUpdate(
 		@Query() query: { version: string },
 		@Req() req: Request
-	) {
+	): Promise<GetVersionUpdateDTO> {
 		const protocol = req.protocol;
 		const host = req.get('host');
 		const urlPrefix = `${protocol}://${host}`;
